12ef83f6e88beb4dd1a30d5d525896e2925b2eda
Miklos Maroti <mmaroti@gmail.com>
2020-04-30 04:16:36 +0200

    first working version, yay

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,172 @@
+extern crate libc;
+use std::ffi::CString;
+use std::io::{Error, ErrorKind};
+use std::sync::atomic::{AtomicI32, Ordering};
+use std::{cmp, process, ptr, slice};
+
+/// A unique identifier used to create shared memory mapped files.
+static BUFFER_ID: AtomicI32 = AtomicI32::new(0);
+
+/// A raw circular buffer of bytes.
+pub struct Buffer {
+    ptr: *mut libc::c_void,
+    size: usize,
+}
+
+impl Buffer {
+    /// Returns the page size of the underlying operating system.
+    pub fn page_size() -> Result<usize, Error> {
+        let page = unsafe { libc::sysconf(libc::_SC_PAGESIZE) };
+        if page <= 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(page as usize)
+        }
+    }
+
+    /// Creates a new circular buffer with the given size. The returned
+    /// size will be rounded up to a multiple of the page size.
+    pub fn new(mut size: usize) -> Result<Buffer, Error> {
+        let page = Buffer::page_size()?;
+
+        // round up to a multiple of the minimum size, be safe
+        size = cmp::max(size, page);
+        size = ((size + page - 1) / page) * page;
+        if size > (i32::max_value() / 2) as usize {
+            return Err(Error::new(ErrorKind::InvalidInput, "invalid length"));
+        }
+
+        // create temporary shared memory file
+        let name = CString::new(format!(
+            "rust-vmcircbuf-{}-{}",
+            process::id(),
+            BUFFER_ID.fetch_add(1, Ordering::Relaxed)
+        ))?;
+        let file_desc = unsafe {
+            libc::shm_open(
+                name.as_ptr(),
+                libc::O_RDWR | libc::O_CREAT | libc::O_EXCL,
+                0o600,
+            )
+        };
+        if file_desc < 0 {
+            return Err(Error::last_os_error());
+        }
+
+        // resize the file to length
+        let ret = unsafe { libc::ftruncate(file_desc, size as libc::off_t) };
+        if ret != 0 {
+            let ret = Error::last_os_error();
+            unsafe { libc::close(file_desc) };
+            return Err(ret);
+        }
+
+        // map with double length
+        let first_copy = unsafe {
+            libc::mmap(
+                ptr::null_mut(),
+                2 * size,
+                libc::PROT_READ | libc::PROT_WRITE,
+                libc::MAP_SHARED,
+                file_desc,
+                0,
+            )
+        };
+        if first_copy == libc::MAP_FAILED {
+            let ret = Error::last_os_error();
+            unsafe { libc::close(file_desc) };
+            return Err(ret);
+        }
+
+        // unmap the second half
+        let ret = unsafe { libc::munmap(first_copy.add(size), size) };
+        if ret != 0 {
+            let ret = Error::last_os_error();
+            unsafe { libc::close(file_desc) };
+            return Err(ret);
+        }
+
+        // memory map the file again
+        let second_copy = unsafe {
+            libc::mmap(
+                first_copy.add(size),
+                size,
+                libc::PROT_READ | libc::PROT_WRITE,
+                libc::MAP_SHARED,
+                file_desc,
+                0,
+            )
+        };
+        if second_copy == libc::MAP_FAILED {
+            let ret = Error::last_os_error();
+            unsafe { libc::close(file_desc) };
+            return Err(ret);
+        } else if second_copy != unsafe { first_copy.add(size) } {
+            unsafe { libc::close(file_desc) };
+            return Err(Error::new(ErrorKind::InvalidData, "second mmap"));
+        }
+
+        // close the file descriptor
+        let ret = unsafe { libc::close(file_desc) };
+        if ret != 0 {
+            return Err(Error::last_os_error());
+        }
+
+        // unlink the shared memory
+        let ret = unsafe { libc::shm_unlink(name.as_ptr()) };
+        if ret != 0 {
+            return Err(Error::last_os_error());
+        }
+
+        Ok(Buffer {
+            ptr: first_copy,
+            size,
+        })
+    }
+
+    /// Returns the size of the circular buffer.
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    /// Returns an immutable slice of the circular buffer starting at `start`
+    /// and containing `count` many elements. Both the `start` and `count`
+    /// parameters must be less than or equal to the `size` of the buffer.
+    /// If `start + count` is bigger than `size`, then the returned slice
+    /// will magically wrap over to the beginning.
+    pub fn slice(&self, start: usize, count: usize) -> &[u8] {
+        assert!(start <= self.size && count <= self.size);
+        unsafe { slice::from_raw_parts(self.ptr.add(start) as *const u8, count) }
+    }
+
+    /// This is the mutable analog of the `slice` method.
+    pub fn slice_mut(&mut self, start: usize, count: usize) -> &mut [u8] {
+        assert!(start <= self.size && count <= self.size);
+        unsafe { slice::from_raw_parts_mut(self.ptr.add(start) as *mut u8, count) }
+    }
+}
+
+impl Drop for Buffer {
+    fn drop(&mut self) {
+        let ret = unsafe { libc::munmap(self.ptr, 2 * self.size) };
+        assert_eq!(ret, 0);
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut buffer = Buffer::new(0).unwrap();
+        let size = buffer.size();
+        println!("buffer length: {}", size);
+        for (i, a) in buffer.slice_mut(0, size).iter_mut().enumerate() {
+            *a = i as u8;
+        }
+        for (i, a) in buffer.slice_mut(1, size).iter_mut().enumerate() {
+            assert_eq!(*a, ((i + 1) % size) as u8);
+        }
     }
 }
